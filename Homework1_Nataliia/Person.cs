b5a46a736452bb803fcda6deb62b3740fc81f609
vkarnau <vkarnau@5CD116MK0L>
2023-06-01 13:46:40 +0200

    Add project files.

---
+++ Homework1_Nataliia/Person.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Homework1_Nataliia
+{
+    public class Person
+    {
+        private string _fn;
+        private string _ln;
+        private DateTime _dob;
+        private string _inn;
+        public string FN
+        {
+            get { return _fn; }
+            set
+            {
+                if (value.Length > 30)
+                {
+                    Console.WriteLine("first name should be no more than 30 symbols");
+                }
+                else if (value.Length < 1)
+                {
+                    Console.WriteLine("first name should contains at least 1 symbol");
+                }
+                else if (!Regex.IsMatch(value, "^[a-zA-Z]"))
+                {
+                    Console.WriteLine("only latin symbols are allowed");
+                }
+                else
+                {
+                    _fn = value;
+                }
+            }
+        }
+
+        public string LN
+        {
+            get { return _ln; }
+            set
+            {
+                if (value.Length > 30)
+                {
+                    Console.WriteLine("last name should be no more than 30 symbols");
+                }
+                else if (value.Length < 1)
+                {
+                    Console.WriteLine("last name should contains at least 1 symbol");
+                }
+                else if (!Regex.IsMatch(value, "^[a-zA-Z]"))
+                {
+                    Console.WriteLine("only latin symbols are allowed");
+                }
+                else
+                {
+                    _ln = value;
+                }
+            }
+        }
+        public DateTime DOB
+        {
+            get { return _dob; }
+            set
+            {
+                if (value.Date > DateTime.Now)
+                {
+                    Console.WriteLine("DOB should be not later that today date");
+                }
+                else
+                {
+                    _dob = value;
+                }
+            }
+        }
+        public string INN
+        {
+            get { return _inn; }
+            set
+            {
+                if (!Regex.IsMatch(value, "^[A-Z]{2}[0-9]{10}$"))
+                {
+                    Console.WriteLine("INN must satisfy pattern: < upper_case >< upper_case >< 10digit >");
+                }
+                else { _inn = value; }
+            }
+        }
+    }
+}
+
+
+
+
---
+++ Homework1_Nataliia/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Homework1_Nataliia;
+
+Console.WriteLine("Hello, World!");
+
+Person person = new Person();
+Person person2 = new Person();
+Person person3 = new Person();
+Person person4 = new Person();
+
+person. LN= "fdg";
+Console.WriteLine($"{person.LN}");
+person2.LN = "fdgghggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg";
+Console.WriteLine($"{person2.LN}");
+person3.LN = "";
+Console.WriteLine($"{person3.LN}");
+person4.LN = "Папуга";
+Console.WriteLine($"{person4.LN}");
+
+
+person.FN = "fdg";
+Console.WriteLine($"{person.FN}");
+person2.FN = "fdgghggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg";
+Console.WriteLine($"{person2.FN}");
+person3.FN = "";
+Console.WriteLine($"{person3.FN}");
+person4.FN = "Білка";
+Console.WriteLine($"{person4.FN}");
+
+person.DOB = new DateTime(2022, 01, 01);
+Console.WriteLine($" good time {person.DOB}");
+person2.DOB = new DateTime(2024, 01, 01);
+Console.WriteLine($" good time {person2.DOB}");
+
+person.INN = "YU8434567898";
+Console.WriteLine($"CORRECT {person.INN}");
+person2.INN = "YUU843456789822";
+Console.WriteLine($" INCORRECT {person2.INN}");
